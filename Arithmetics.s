2add4bdc8547b9be5bf1a95964c5a20f6bec71ae
ThePernalonga <62114761+ThePernalonga@users.noreply.github.com>
2021-04-30 11:10:40 -0300

    Add

---
+++ Arithmetics.s
@@ -0,0 +1,66 @@
+@ Just some codes with sum, sub, multiply and function uses
+@ Also showing in display
+
+.equ PrintS,0x204                       @ Bind to print a String on the LCD
+.equ Print,0x205                        @ Bind to print an Integer on the LCD
+
+.text                                   
+
+    str_ini: .asciz "These are some arithmetic functions:"
+    str_sum: .asciz "The Addiction of 1 and 2 is: "
+    str_sub: .asciz "The Subtraction of 2 and 1 is: "
+    str_mul: .asciz "The Multiplication of 1 and 2 is: "
+
+.align                                  
+
+@ Setting up some Integers to use on functions
+mov r9, #1                              @ Set as value of 1 (r9 = 1)
+mov r8, #2                              @ Set as value of 2 (r9 = 2)
+
+
+Start:
+    mov r0, #0                          @ Alloc r0 to position 'x' 0, or variable r0 recive 0 (r0 = 0)
+    mov r1, #0                          @ Alloc r1 to position 'y' 0, or variable r1 recive 0 (r1 = 0)
+    ldr r2, =str_ini                    @ Alloc a string value to r2 (exclusivelly for string))
+    swi PrintS                          @ Will use r0, r1 and r2 as info to print
+
+Sum:
+    mov r0, #0                          @ Alloc r0 to position 'x' 0, or variable r0 recive 0 (r0 = 0)
+                                        @ In this case you don't need to set 0 on r0, it's already in it from 'Start'
+    mov r1, #1                          @ Alloc r1 to position 'y' 0, or variable r1 recive 0 (r1 = 1)
+    ldr r2, =str_sum                    @ Setting the string "str_sum" in r2 (r2 = str_sum)
+    swi PrintS                          @ Values set on those lines above
+
+    mov r1, #2                          @ Moving 'y' to position 2 (y = 2)
+    add r7, r8, r9                      @ 'add' in r7 the sum os r8 and r9 (r7 = r8 + r9)
+    mov r2, r7                          @ Putting in r2 the value of r7, will be used to print
+    swi Print                           @ Print the integer in r2
+
+
+Sub:
+    mov r0, #0                          @ Alloc r0 to position 'x' 0, or variable r0 recive 0 (r0 = 0)
+                                        @ In this case you don't need to set 0 on r0, it's already in it from 'Start'
+    mov r1, #3                          @ Alloc r1 to position 'y' 3, or variable r1 recive 0 (r1 = 3)
+    ldr r2, =str_sub                    @ Setting the string "str_sub" in r2 (r2 = str_sub)
+    swi PrintS                          @ Values set on those lines above
+
+    mov r1, #4                          @ Moving 'y' to position 4 (y = 4)
+    sub r7, r9, r8                      @ 'sub' the r8 from r9 and putting in r7 (r7 = r9 - r8)
+    mov r2, r7                          @ Putting in r2 the value os r7, will be used to print
+    swi Print                           @ Print the integer in r2
+
+
+Mult:
+    mov r0, #0                          @ Alloc r0 to position 'x' 0, or variable r0 recive 0 (r0 = 0)
+                                        @ In this case you don't need to set 0 on r0, it's already in it from 'Start'
+    mov r1, #5                          @ Alloc r1 to position 'y' 5, or variable r1 recive 0 (r1 = 5)
+    ldr r2, =str_mul                    @ Setting the string "str_mul" in r2 (r2 = str_sub)
+    swi PrintS                          @ Values set on those lines above
+
+    mov r1, #6                          @ Moving 'y' to position 6 (y = 6)
+    mul r7, r9, r8                      @ 'mul' the r8 from r9 and putting in r7 (r7 = r9 * r8)
+    mov r2, r7                          @ Putting in r2 the value os r7, will be used to print
+    swi Print                           @ Print the integer in r2
+
+
+.end
--- HelloWorld.s
+++ HelloWorld.s
@@ -12,8 +12,10 @@
 .align                                  @ This specify where the code is
 
 start:                                  @ This is a "Function", you can name it what ever you want
-    mov r0, #0                          @ Alloc r0 to position 0, or variable r0 recive 0 (r0 = 0)
-    mov r1, #0                          @ Alloc r1 to position 0, or variable r1 recive 0 (r1 = 0)
+
+            @ #Number means a Integer value
+    mov r0, #0                          @ Alloc r0 to position 'x' 0, or variable r0 recive 0 (r0 = 0)
+    mov r1, #0                          @ Alloc r1 to position 'y' 0, or variable r1 recive 0 (r1 = 0)
     ldr r2, =str_hello                  @ Alloc a string value to r2
     swi Print                           @ Will use r0, r1 and r2 as info to print
 
