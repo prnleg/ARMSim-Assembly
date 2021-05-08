b9bdd73f9677bec34edd7137a8b99a60e4ba36b5
ThePernalonga <62114761+ThePernalonga@users.noreply.github.com>
2021-05-08 18:40:09 -0300

    Blue Button n' 8 Segment Display

---
+++ Bottons-8SegDisplay.s
@@ -0,0 +1,215 @@
+@ Just a code to use the buttons 'Left' and 'Right'
+@ and the 0.0 to 3.3
+@ Are there better solutions for this use in specific, just use as example
+
+
+@ Those are bind to use the Software Interuptions Quickier
+.equ Seg8, 0x200                                    @ Display on the 8 Segments the value in r0
+.equ Print, 0x205                                   @ Display on the LCD the value in r2
+.equ Bottn, 0x202                                   @ Put in r0 the button pressed (left/right)
+.equ BlueB, 0x203                                   @ Put in r0 the button pressed (0-15)
+
+
+@ Those are binds for each one on 8 segment display 
+.equ SEG_A,0x80
+.equ SEG_B,0x40
+.equ SEG_C,0x20
+.equ SEG_P,0x10
+.equ SEG_D,0x08
+.equ SEG_E,0x04
+.equ SEG_F,0x02
+.equ SEG_G,0x01
+
+.text
+
+Digits:                                             @ Those are the value for r0:
+                                                    @ that will also be on display
+    .word SEG_A|SEG_B|SEG_C|SEG_D|SEG_E|SEG_G       @ 0
+    .word SEG_B|SEG_C                               @ 1
+    .word SEG_A|SEG_B|SEG_F|SEG_E|SEG_D             @ 2
+    .word SEG_A|SEG_B|SEG_F|SEG_C|SEG_D             @ 3
+    .word SEG_G|SEG_F|SEG_B|SEG_C                   @ 4
+    .word SEG_A|SEG_G|SEG_F|SEG_C|SEG_D             @ 5
+    .word SEG_A|SEG_G|SEG_F|SEG_E|SEG_D|SEG_C       @ 6
+    .word SEG_A|SEG_B|SEG_C                         @ 7
+    .word SEG_A|SEG_B|SEG_C|SEG_D|SEG_E|SEG_F|SEG_G @ 8
+    .word SEG_A|SEG_B|SEG_F|SEG_G|SEG_C             @ 9
+    .word SEG_A|SEG_G|SEG_B|SEG_F|SEG_E|SEG_C       @ A [10]
+    .word SEG_G|SEG_E|SEG_F|SEG_D|SEG_C             @ B [11]
+    .word SEG_A|SEG_G|SEG_E|SEG_D                   @ C [12]
+    .word SEG_B|SEG_E|SEG_F|SEG_D|SEG_C             @ D [13]
+    .word SEG_A|SEG_D|SEG_E|SEG_F|SEG_G             @ E [14]
+    .word SEG_A|SEG_E|SEG_F|SEG_G                   @ F [15]
+    .word 0                                         @ 'empty'
+
+
+.align
+
+mov r0, #0                                          @ Starting the variable with 0
+mov r1, #0                                          @ Starting the variable with 0
+mov r2, #0                                          @ Starting the variable with 0
+
+
+@ This will create a loop where it compares if any Blue Button was pressed or not
+Start:
+    swi BlueB                                       @ Puts in r0 the value of the button pressed
+    cmp r0, #0
+    bne Display
+    beq Start                                       @ Here's where the loop starts again
+
+
+@ This is where it compares with each specific buttton, there are better ways to do that
+@ Use this just as an example
+Display:
+    cmp r0, #0x01
+    beq Dig0
+    cmp r0, #0x02
+    beq Dig1
+    cmp r0, #0x04
+    beq Dig2
+    cmp r0, #0x08
+    beq Dig3
+    cmp r0, #0x10
+    beq Dig4
+    cmp r0, #0x20
+    beq Dig5
+    cmp r0, #0x40
+    beq Dig6
+    cmp r0, #0x80
+    beq Dig7
+    cmp r0, #0x100
+    beq Dig8
+    cmp r0, #0x200
+    beq Dig9
+    cmp r0, #0x400
+    beq DigA
+    cmp r0, #0x800
+    beq DigB
+    cmp r0, #0x1000
+    beq DigC
+    cmp r0, #0x2000
+    beq DigD
+    cmp r0, #0x4000
+    beq DigE
+    cmp r0, #0x8000
+    beq DigF
+    bne Start
+
+
+@ Each function is printing a number (in hexa) on the 8 Segment Display
+Dig0:
+    mov r3, #0                                      @ As the macro runs, it will pull the position '0' in this case
+    ldr r1, =Digits                                 @ Load the macros on r1
+    ldr r0, [r1, r3, lsl#2]                         @ Puts on r0 the digits and the position you choose
+    swi Seg8                                        @ Print on the Display
+    b Start                                         @ Return to the code loop
+
+
+@ Down below will be the same "base", just changing the position
+
+Dig1:
+    mov r3, #1
+    ldr r1, =Digits
+    ldr r0, [r1, r3, lsl#2]
+    swi Seg8
+    b Start
+
+Dig2:
+    mov r3, #2
+    ldr r1, =Digits
+    ldr r0, [r1, r3, lsl#2]
+    swi Seg8
+    b Start
+
+Dig3:
+    mov r3, #3
+    ldr r1, =Digits
+    ldr r0, [r1, r3, lsl#2]
+    swi Seg8
+    b Start
+
+Dig4:
+    mov r3, #4
+    ldr r1, =Digits
+    ldr r0, [r1, r3, lsl#2]
+    swi Seg8
+    b Start
+
+Dig5:
+    mov r3, #5
+    ldr r1, =Digits
+    ldr r0, [r1, r3, lsl#2]
+    swi Seg8
+    b Start
+
+Dig6:
+    mov r3, #6
+    ldr r1, =Digits
+    ldr r0, [r1, r3, lsl#2]
+    swi Seg8
+    b Start
+
+Dig7:
+    mov r3, #7
+    ldr r1, =Digits
+    ldr r0, [r1, r3, lsl#2]
+    swi Seg8
+    b Start
+
+Dig8:
+    mov r3, #8
+    ldr r1, =Digits
+    ldr r0, [r1, r3, lsl#2]
+    swi Seg8
+    b Start
+
+Dig9:
+    mov r3, #9
+    ldr r1, =Digits
+    ldr r0, [r1, r3, lsl#2]
+    swi Seg8
+    b Start
+
+DigA:
+    mov r3, #10
+    ldr r1, =Digits
+    ldr r0, [r1, r3, lsl#2]
+    swi Seg8
+    b Start
+
+DigB:
+    mov r3, #11
+    ldr r1, =Digits
+    ldr r0, [r1, r3, lsl#2]
+    swi Seg8
+    b Start
+
+DigC:
+    mov r3, #12
+    ldr r1, =Digits
+    ldr r0, [r1, r3, lsl#2]
+    swi Seg8
+    b Start
+
+DigD:
+    mov r3, #13
+    ldr r1, =Digits
+    ldr r0, [r1, r3, lsl#2]
+    swi Seg8
+    b Start
+
+DigE:
+    mov r3, #14
+    ldr r1, =Digits
+    ldr r0, [r1, r3, lsl#2]
+    swi Seg8
+    b Start
+
+DigF:
+    mov r3, #15
+    ldr r1, =Digits
+    ldr r0, [r1, r3, lsl#2]
+    swi Seg8
+    b Start
+
+.end
